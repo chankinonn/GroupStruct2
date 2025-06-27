94941fd4db421733531cbcdbe43aff0471c36f61
chankinonn <chankinonn@users.noreply.github.com>
2025-06-26 20:27:19 -0500

    updated dapc

--- inst/app/modules/meristic/mod_visual.R
+++ inst/app/modules/meristic/mod_visual.R
@@ -111,7 +111,7 @@ mod_visual_ui_meristic <- function(id) {
                                               choices = c("Circle" = 19, "Square" = 15, "Triangle" = 17, "Diamond" = 18),
                                               selected = 19),
                                   uiOutput(ns("n_pca_dapc_ui")),
-                                  sliderInput(ns("n_da_dapc"), "Number of Discriminant Axes (n.da). Download the PCA summary table from the PCA tab and retain the number of PCs that explain 80-90% of total variance:", min = 1, max = 5, value = 2, step = 1),
+                                  sliderInput(ns("n_da_dapc"), "Number of Discriminant Axes (n.da):", min = 1, max = 5, value = 2, step = 1),
                                   checkboxInput(ns("dapc_ellipse"), "Show 67% Confidence Ellipses (following adegenet)", value = TRUE),
                                   checkboxInput(ns("dapc_convex"), "Show Convex Hulls", value = FALSE),
                                   checkboxInput(ns("dapc_centroids"), "Show Group Centroids", value = FALSE),
@@ -156,7 +156,7 @@ mod_visual_server_meristic <- function(id, dataset,
     output$n_pca_dapc_ui <- renderUI({
       req(dataset())
       n_traits <- ncol(dataset()) - 1
-      sliderInput(ns("n_pca_dapc"), "Number of PCA Components (n.pca):",
+      sliderInput(ns("n_pca_dapc"), "Number of PCA Components (n.pca). Download the PCA summary table from the PCA tab and retain the number of PCs that explain 80-90% of total variance:",
                   min = 1, max = n_traits, value = min(5, n_traits), step = 1)
     })
     
